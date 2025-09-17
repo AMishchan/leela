c9e9b8b5998242704c2711ae59d987cda0c0c94e
Mishchan Aleksandr <zayander@gmail.com>
2025-09-17 08:51:35 +0200

    added weebhook to catch dice responce

--- leela/settings.py
+++ leela/settings.py
@@ -127,7 +127,10 @@ ALLOWED_HOSTS = ["142.132.210.43", "localhost", "127.0.0.1", "::1"]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "static_collected"   # отдельная папка для collectstatic
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
